4264ea5c79e85abc06f5e315b15a1815467fca20
SeongHoon Wee <mrgravity817@gmail.com>
2022-02-25 21:45:56 +0900

    Learn Consumer Errors and Tips

--- lib/main.dart
+++ lib/main.dart
@@ -1,142 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:provider_overview_01/babies.dart';
-import 'package:provider_overview_01/dog.dart';
 
-void main() {
+void main(List<String> args) {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<Dog>(
-          create: (context) => Dog(name: 'dog06', breed: 'breed06', age: 5),
-        ),
-        FutureProvider<int>(
-            create: (context) {
-              // Since this executes only once, we cannot use `context.watch()` here
-              final int dogAge = context.read<Dog>().age;
-              final babies = Babies(age: dogAge);
-              return babies.getBabies();
-            },
-            initialData: 0),
-        StreamProvider(
-            create: (context) {
-              final int dogAge = context.read<Dog>().age;
-              final babies = Babies(age: dogAge * 2);
-              return babies.bark();
-            },
-            initialData: "Bark 0 times")
-      ],
-      child: MaterialApp(
-        title: 'Provider 06',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const MyHomePage(),
-      ),
+    return MaterialApp(
+      title: "Provider 09",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class Foo with ChangeNotifier {
+  String value = "Foo";
+  void changeValue() {
+    value = value == "Foo" ? "Bar" : "Foo";
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final dog = Dog(name: 'dog03', breed: 'breed03');
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Provider 06'),
-        ),
-        body: Consumer<Dog>(
-            builder: (BuildContext context, Dog dog, Widget? child) {
-              return Center(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  // `!` should be written behind `child` because it can be null
-                  child!,
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  Text("- name: ${dog.name}",
-                      style: const TextStyle(fontSize: 20.0)),
-                  const SizedBox(height: 10.0),
-                  const BreedAndAge()
-                ],
-              ));
-            },
-            // Since this don't need to be rerendered by context change, it should be in `child` props
-            child: const Text("I like dogs very much",
-                style: TextStyle(fontSize: 20.0))));
-  }
-}
-
-class BreedAndAge extends StatelessWidget {
-  const BreedAndAge({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<Dog>(
-      builder: (_, Dog dog, __) {
-        return Column(
-          children: [
-            Text('- breed: ${dog.breed}',
-                style: const TextStyle(fontSize: 20.0)),
-            const SizedBox(height: 10.0),
-            const Age(),
-          ],
-        );
-      },
-    );
-  }
-}
-
-class Age extends StatelessWidget {
-  const Age({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<Dog>(
-      builder: (_, Dog dog, __) {
-        return Column(
-          children: [
-            Text('- age: ${dog.age}', style: const TextStyle(fontSize: 20.0)),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Text('- number of babies: ${context.read<int>()}',
-                style: const TextStyle(fontSize: 20.0)),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Text('- number of barks: ${context.watch<String>()}',
-                style: const TextStyle(fontSize: 20.0)),
-            const SizedBox(
-              height: 20.0,
-            ),
-            ElevatedButton(
-                onPressed: () => dog.grow(),
-                child: const Text('Grow', style: TextStyle(fontSize: 20.0)))
-          ],
-        );
-      },
-    );
+        appBar: AppBar(title: Text('Provider 09')),
+        body: ChangeNotifierProvider<Foo>(
+            create: (_) => Foo(),
+            child: Consumer<Foo>(
+              builder: (BuildContext context, Foo foo, Widget? child) => Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('${foo.value}', style: TextStyle(fontSize: 40)),
+                      SizedBox(height: 20.0),
+                      ElevatedButton(
+                          onPressed: () => context.read<Foo>().changeValue(),
+                          child: Text('Change Value',
+                              style: TextStyle(fontSize: 20)))
+                    ]),
+              ),
+            )));
   }
 }
